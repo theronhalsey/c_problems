f93bc8b2d9da652f7e9f2af64d823c610eac1909
Theron <theron.halsey@gmail.com>
2025-05-08 14:40:36 -0400

    added random int generator

---
+++ randNinRange.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    srand(time(NULL));
+    uint64_t max = 10;
+
+    for (int i = 0; i < 20; i++)
+    {
+        uint32_t r = (rand() << 16) + rand();
+        printf("N: %d\n", ((uint64_t)(r * max) >> 31) + 1);
+    }
+
+    return 0;
+}
